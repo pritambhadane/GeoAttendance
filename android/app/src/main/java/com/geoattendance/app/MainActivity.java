a7600bbfec838e280366103462536cd70b0bd2f6
pritambhadane <pritambhadane@gmail.com>
2026-06-08 13:51:25 +0530

    fix: register BackgroundGeolocationPlugin in MainActivity

--- android/app/src/main/java/com/geoattendance/app/MainActivity.java
+++ android/app/src/main/java/com/geoattendance/app/MainActivity.java
@@ -1,5 +1,13 @@
 package com.geoattendance.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.equimapper.capacitor.plugin.backgroundgeolocation.BackgroundGeolocationPlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(BackgroundGeolocationPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
